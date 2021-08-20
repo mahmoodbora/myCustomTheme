4bf83433bb9f75ace868b5b2256897264378155b
Mahmood Bora <mahmood.bora@gmail.com>
2021-08-20 15:35:55 +0400

    This is my First Commit with basic files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="container">
+    <section class="header">
+        <div class="customlogo"></div>
+        <div class="search"></div>
+        <nav class="mainmenu"></nav>
+    </section>
+    <section class="banner">
+        <div class="bannercontent"></div>
+        <aside class="content"></aside>
+    </section>
+    <section class="footer">
+        <box class="boxcontent"></box>
+        <box class="boxcontent"></box>
+        <box class="boxcontent"></box>
+        <box class="boxcontent"></box>
+    </section>
+    <section class="powered"></section>
+    </div>
+</body>
+</html>
